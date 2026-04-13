21fa2bb7678ecd09309dcf69cf331756dd74e7cc
车木 <kaiusgs@chemudeMacBook-Pro.local>
2026-04-13 15:22:39 +0100

    add extracted data values

---
+++ script.R
@@ -0,0 +1,13 @@
+time_data <- c(0, 2, 4, 5, 6, 7, 12)# day-degrees/100
+S_data    <- c(0.0, 5.4, 3.8, 3.9, 3.9, 3.5, 3.3)# susceptible stems
+I_data    <- c(0.0, 0.1, 0.3, 0.3, 0.5, 0.5, 0.2) # infected stems
+
+# Table 2 parameters for Model III Low density
+b      <- 0.0005
+kappa  <- 11.725
+lambda0 <- 0.064
+mu     <- 0.140
+alpha1 <- 0.721
+alpha2 <- 0.261
+alpha3 <- 0.0003
+d      <- 0.215
